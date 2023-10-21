9c08f6723bc258b9236e24d3a5b45d5bf06d84b5
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-10-21 12:45:19 +0300

    create FullInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React, {useState} from 'react';
 import './App.css';
+import {FullInput} from "./components/FullInput";
 
 function App() {
-  const [message, setMessage] = useState([
-        {message: 'message1'},
-        {message: 'message2'},
-        {message: 'message3'}
-      ]
-  )
-  return (
-      <div className="App">
-        <div>
-          <input/>
-          <button>+</button>
+    const [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'}
+        ]
+    )
+
+    const addMessage = (title: string) => {
+        let newMessage = {message: title}
+        setMessage([newMessage,...message])
+    }
+
+    return (
+        <div className="App">
+            <FullInput addMessage={addMessage}/>
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
         </div>
-        {message.map((el, index) => {
-          return (
-              <div key={index}>{el.message}</div>
-          )
-        })}
-      </div>
-  );
+    );
 }
 
 export default App;
